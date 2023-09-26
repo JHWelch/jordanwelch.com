941956302826bc9d8b24352315ec303a36a1a722
JHWelch <JHWelch@users.noreply.github.com>
2023-09-26 04:35:43 +0000

    deploy: d980cdb77f15811e628667fd22c6cec48445e395

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'share-tech': ['"Share Tech"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
